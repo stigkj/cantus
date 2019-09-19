8e74bebebc1805366672b5018b267d74d0a108bc
Martin Rødland <martin.rodland@skatteetaten.no>
2019-09-19 10:42:40 +0200

    feature/AOS-3614 Nexus IQ (#31)
    * added nexus iq to jenkinsfile, bumped dependencies, changed distributionUrl
    
    * updated gradle wrapper files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
     id("org.springframework.cloud.contract")
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.41"
+    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
     id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.sonarqube") version "2.7.1"
 
-    id("org.springframework.boot") version "2.1.7.RELEASE"
+    id("org.springframework.boot") version "2.1.8.RELEASE"
     id("org.asciidoctor.convert") version "2.3.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.0.0"
-    id("com.github.ben-manes.versions") version "0.22.0"
+    id("com.github.ben-manes.versions") version "0.25.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.12"
 
     id("no.skatteetaten.gradle.aurora") version "2.4.2"
@@ -17,10 +17,10 @@ plugins {
 
 dependencies {
     implementation("uk.q3c.rest:hal-kotlin:0.5.4.0.db32476")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.2.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.3.41")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.3.50")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("io.projectreactor.addons:reactor-extra:3.2.3.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-web")
